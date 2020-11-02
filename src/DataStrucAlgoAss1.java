1e1749fa2dec89a9dbc83c821602bffb37bd371b
Chris Galindo <chris.ray.galindo@gmail.com>
2020-11-02 05:05:40 -0600

    CourseraAss1

---
+++ src/DataStrucAlgoAss1.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Brackets {
+
+	IsBalanced(str)//
+	Stack<E> stack;
+	
+	for char in str;
+			
+		if char in ['(, '[''];
+			
+			stack.Push(char);
+			
+			else:
+				
+				if stack.Empty();
+					retrn False;
+					top <- stack.Pop();
+					
+				if (top = '[' and char != ']') or
+						(top = '(' and char != ')'):
+							
+				return False;
+				return stack.empty();
+	
+}
